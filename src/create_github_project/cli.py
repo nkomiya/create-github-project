4de12eb2a5e3c5264b1ae7d18a1e4dc26b64399f
nkomiya <n.comiya@gmail.com>
2021-07-23 22:45:53 +0900

    add validation for branch name

--- src/create_github_project/cli.py
+++ src/create_github_project/cli.py
@@ -21,17 +21,24 @@ def cmd() -> None:
 @click.option('--production', type=str, default=None,
               help='Production branch name.')
 def init(repo_dir: Path, production: Union[str, None]) -> None:
-    """ローカルリポジトリを初期化用のコマンド。
+    """ローカルリポジトリを初期化するコマンド。
 
     Args:
         repo_dir (str): リポジトリ作成先
         production (Union[str, None]): 本番用ブランチの名前
     """
+    # リポジトリ作成先にフォルダ/ファイルが存在しないこと
     if repo_dir.exists():
         raise click.BadParameter(f'Directory {repo_dir.as_posix()} already exists.')
+
+    # ブランチ名が未入力の場合
     if production is None:
         production = questionary.text(f'Production branch name [{_DEFAULT_PRODUCTION_BRANCH}]?').ask()
         production = production or _DEFAULT_PRODUCTION_BRANCH
+
+    # ブランチ名のチェック
+    if not ResourceManager.validate_branch(production):
+        raise click.BadParameter(f"Branch name '{production}' is not allowed for production branch.")
 
     rm = ResourceManager(repo_dir, production)
     rm.initialize()
