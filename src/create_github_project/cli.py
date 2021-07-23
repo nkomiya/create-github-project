7e5cda5845c237f6bd86b607ae0da3070e1a8e12
nkomiya <n.comiya@gmail.com>
2021-07-23 22:19:54 +0900

    feat: add remote repository prompt

--- src/create_github_project/cli.py
+++ src/create_github_project/cli.py
@@ -1,8 +1,12 @@
 from pathlib import Path
+from typing import Union
 
 import click
+import questionary
 
 from create_github_project.resource_manager import ResourceManager
+
+_DEFAULT_PRODUCTION_BRANCH = 'master'
 
 
 @click.group()
@@ -12,18 +16,22 @@ def cmd() -> None:
     pass
 
 
-@cmd.command(help="ローカルリポジトリを初期化する。")
+@cmd.command(help="Initialize local Git repository.")
 @click.argument('repo_dir', type=click.Path(exists=False, path_type=Path))
-@click.option('--production', type=str, default='master',
+@click.option('--production', type=str, default=None,
               help='Production branch name.')
-def init(repo_dir: Path, production: str) -> None:
+def init(repo_dir: Path, production: Union[str, None]) -> None:
     """ローカルリポジトリを初期化用のコマンド。
 
     Args:
         repo_dir (str): リポジトリ作成先
-        production (str): 本番用ブランチの名前
+        production (Union[str, None]): 本番用ブランチの名前
     """
     if repo_dir.exists():
         raise click.BadParameter(f'Directory {repo_dir.as_posix()} already exists.')
+    if production is None:
+        production = questionary.text(f'Production branch name [{_DEFAULT_PRODUCTION_BRANCH}]?').ask()
+        production = production or _DEFAULT_PRODUCTION_BRANCH
+
     rm = ResourceManager(repo_dir, production)
     rm.initialize()
