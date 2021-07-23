c4f15ab588993d066d982714e32c2697645b9810
nkomiya <38785560+nkomiya@users.noreply.github.com>
2021-07-23 16:01:15 +0900

    feat: add support for initializing git repository (#4)

--- src/create_github_project/cli.py
+++ src/create_github_project/cli.py
@@ -1,2 +1,26 @@
+from pathlib import Path
+
+import git
+
+REPO_DIR = 'template'
+PRODUCTION_BRANCH = 'master'
+DEVELOP_BRANCH = 'develop'
+COMMIT_MESSAGE = 'chore: initialize repository'
+
+
 def main():
-    print('test')
+    # リポジトリ作成
+    repo = git.Repo.init(REPO_DIR)
+    repo.git.checkout(b=PRODUCTION_BRANCH)
+
+    # TODO: 適切なファイルパスに変更する
+    root = Path(REPO_DIR)
+    readme = root.joinpath('README.md')
+    readme.touch()
+
+    # commit
+    repo.index.add([readme.relative_to(REPO_DIR).as_posix()])
+    repo.index.commit(COMMIT_MESSAGE)
+
+    # develop ブランチの作成
+    repo.git.checkout(PRODUCTION_BRANCH, b=DEVELOP_BRANCH)
